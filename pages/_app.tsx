349f103b7c555795126f67853015ecd49827a331
Alex <alexfilsecker@gmail.com>
2023-12-22 12:04:31 -0300

    mui with default theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,9 @@
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import type { AppProps } from 'next/app';
-import theme from '../styles/theme';
 
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={createTheme()}>
       <CssBaseline />
       <Component {...pageProps} />
     </ThemeProvider>
