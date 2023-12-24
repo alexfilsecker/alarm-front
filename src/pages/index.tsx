86315c8b944aa609b3aaf4be7a9fe3a283823eef
Alex <alexfilsecker@gmail.com>
2023-12-24 12:15:04 -0300

    tailwind working

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+const IndexPage = (): JSX.Element => (
+  <div className="bg-amber-600 text-red">
+    <h1 className="text-blue">This is my Alarm frontend</h1>
+    <p>Its a work in progress</p>
+  </div>
+);
+
+export default IndexPage;
