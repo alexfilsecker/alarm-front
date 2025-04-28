97ed515ef84d150b9b77e88c0ba5a1784deab3ad
Alex <alexfilsecker@gmail.com>
2025-04-28 01:39:32 -0400

    create API class

---
+++ src/lib/api/index.ts
@@ -0,0 +1,41 @@
+import { PUBLIC_BASE_URL } from '$env/static/public';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+
+export default class API {
+	static async request<T = unknown>(
+		method: 'POST' | 'GET',
+		path: string,
+		body: object | null = null,
+		withToken: boolean = true
+	) {
+		let token: string | undefined;
+		if (withToken) {
+			token = Cookies.get('token');
+			if (token === undefined) {
+				console.error('NO TOKEN');
+				return {};
+			}
+		}
+
+		const response = await axios.request<T>({
+			data: method !== 'GET' ? body : undefined,
+			params: method === 'GET' ? body : undefined,
+			method,
+			url: `${PUBLIC_BASE_URL}/${path}`,
+			headers: {
+				Authorization: withToken ? `Bearer ${token}` : undefined
+			}
+		});
+
+		return response.data;
+	}
+
+	static post<T = unknown>(path: string, body: object | null = null, withToken: boolean = true) {
+		return this.request<T>('POST', path, body, withToken);
+	}
+
+	static async get<T = unknown>(path: string, params: object | null, withToken: boolean = true) {
+		return this.request<T>('GET', path, params, withToken);
+	}
+}
