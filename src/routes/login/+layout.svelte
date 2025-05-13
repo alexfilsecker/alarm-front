567be521ab128f7dd9a2cdcc8100617cae6ba738
Alex <alexfilsecker@gmail.com>
2025-05-13 15:55:41 -0400

    no layout in simple login

--- src/routes/login/+layout.svelte
+++
@@ -1,3 +0,0 @@
-<div class="m-10 flex flex-col items-center gap-6">
-	<slot />
-</div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -26,29 +26,31 @@
 	};
 </script>
 
-<h1 class="text-3xl">LOGIN</h1>
-<div class="flex w-1/2 flex-col gap-5">
-	<div class="flex flex-col gap-2">
-		<div class="flex flex-col gap-1">
-			<Label class="text-xl" for="username">Username</Label>
-			<Input type="text" id="username" bind:value={username} />
-			{#if loginErrors.username !== undefined}
-				<Helper color="red" class="text-md">{loginErrors.username}</Helper>
-			{/if}
+<div class="m-10 flex flex-col items-center gap-6">
+	<h1 class="text-3xl">LOGIN</h1>
+	<div class="flex w-1/2 flex-col gap-5">
+		<div class="flex flex-col gap-2">
+			<div class="flex flex-col gap-1">
+				<Label class="text-xl" for="username">Username</Label>
+				<Input type="text" id="username" bind:value={username} />
+				{#if loginErrors.username !== undefined}
+					<Helper color="red" class="text-md">{loginErrors.username}</Helper>
+				{/if}
+			</div>
+			<div class="flex flex-col gap-1">
+				<Label class="text-xl" for="password">Password</Label>
+				<Input type="password" id="password" bind:value={password} />
+				{#if loginErrors.password !== undefined}
+					<Helper color="red" class="text-md">{loginErrors.password}</Helper>
+				{/if}
+			</div>
 		</div>
-		<div class="flex flex-col gap-1">
-			<Label class="text-xl" for="password">Password</Label>
-			<Input type="password" id="password" bind:value={password} />
-			{#if loginErrors.password !== undefined}
-				<Helper color="red" class="text-md">{loginErrors.password}</Helper>
+		<Button {onclick} {disabled}>
+			{#if loading}
+				Loading...
+			{:else}
+				Login
 			{/if}
-		</div>
+		</Button>
 	</div>
-	<Button {onclick} {disabled}>
-		{#if loading}
-			Loading...
-		{:else}
-			Login
-		{/if}
-	</Button>
 </div>
