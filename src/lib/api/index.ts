8f5b95cb214dd246be1bf3f7a9fb80b5535bfd42
Alex <alexfilsecker@gmail.com>
2025-05-13 21:00:59 -0400

    fixed tokens on http requests

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,6 +1,26 @@
 import axios from 'axios';
 import Cookies from 'js-cookie';
 import { importBaseUrl } from './importBaseUrl';
+import { validateKnownAxiosError } from './axiosErrors';
+import { goto } from '$app/navigation';
+import { refreshTokens } from './refreshTokens';
+
+const badToken = async () => {
+	const refreshToken = Cookies.get('refreshToken');
+	if (refreshToken === undefined) {
+		throw new Error('no refreshToken');
+	}
+
+	const newTokens = await refreshTokens(refreshToken);
+	if (newTokens === null) {
+		throw new Error('Could not refresh tokens');
+	}
+
+	const { newToken, newRefreshToken } = newTokens;
+	Cookies.set('token', newToken);
+	Cookies.set('refreshToken', newRefreshToken);
+	return newToken;
+};
 
 export default class API {
 	static async request<T = unknown>(
@@ -13,25 +33,45 @@ export default class API {
 		if (withToken) {
 			token = Cookies.get('token');
 			if (token === undefined) {
-				// TODO: Should verify if there is a refreshToken and try to get new Tokens
-				// If it can't get new tokens we should redirect to /login
-				throw new Error('No Token');
+				try {
+					token = await badToken();
+				} catch {
+					goto('/login');
+				}
 			}
 		}
 
 		const baseUrl = await importBaseUrl();
 
-		const response = await axios.request<T>({
-			data: method !== 'GET' ? body : undefined,
-			params: method === 'GET' ? body : undefined,
-			method,
-			url: `${baseUrl}/${path}`,
-			headers: {
-				Authorization: withToken ? `Bearer ${token}` : undefined
-			}
-		});
+		let done = false;
+		while (!done) {
+			try {
+				const response = await axios.request<T>({
+					data: method !== 'GET' ? body : undefined,
+					params: method === 'GET' ? body : undefined,
+					method,
+					url: `${baseUrl}/${path}`,
+					headers: {
+						Authorization: withToken ? `Bearer ${token}` : undefined
+					}
+				});
+				return response.data;
+			} catch (e: unknown) {
+				const axiosError = validateKnownAxiosError(e); // Throws the error if it is not AxiosError
+				if (axiosError.status !== 401) {
+					throw e;
+				}
 
-		return response.data;
+				try {
+					token = await badToken();
+				} catch {
+					goto('/login');
+					done = true;
+				}
+			}
+		}
+
+		return null;
 	}
 
 	static post<T = unknown>(path: string, body: object | null = null, withToken: boolean = true) {
